11425ea6fa1f53c4e3746d13548f66bdbe5dbff9
carlosvin <carlosvin@gmail.com>
2014-11-14 14:15:20 +0100

    working with google_maps package

--- web/tracking_element.dart
+++ web/tracking_element.dart
@@ -2,9 +2,9 @@ library tracking;
 
 import 'package:observe/observe.dart';
 import 'package:polymer/polymer.dart';
-import 'dart:js';
 import 'dart:html';
 import 'positioning.dart';
+import 'package:google_maps/google_maps.dart';
 
 @CustomTag('tracking-element')
 class TrackingElement extends PolymerElement {
@@ -13,15 +13,19 @@ class TrackingElement extends PolymerElement {
   @published String borderColor = 'rgb(0,0,0)';
   
   final Positioning positioning = new Positioning();
-
+  GMap map;
+  Element mapCanvas;
   TrackingElement.created() : super.created();
 
   @override
   void attached() {
     super.attached();
+    mapCanvas = $['map-canvas'];//shadowRoot.querySelector('#map-canvas');
     window.navigator.geolocation.watchPosition(enableHighAccuracy: true)
       .listen((Geoposition position) {addPosition(position);},
       onError: (PositionError error) => handleError(error));
+    
+    
   }
   
   @override
@@ -38,9 +42,28 @@ class TrackingElement extends PolymerElement {
     if (positioning.addPosition(geoPosition)){
       speedAverage = "${positioning.speedAvg}km/h";
       gpsStatus = positioning.last.toString();
-      context.callMethod('addPosition', [geoPosition.coords.latitude, geoPosition.coords.longitude]);
+      newPositionInMap();
     }
   }
+  
+  void newPositionInMap(){
+    if (map == null){
+      final mapOptions = new MapOptions()
+          ..zoom = 17
+          ..center = center
+          ..mapTypeId = MapTypeId.ROADMAP
+          ;
+      print("options");
+      map = new GMap(mapCanvas, mapOptions);
+      print("map");
+    }else{
+      map.center = center;   
+    }
+  }
+  
+  num get lat => positioning.last.lat; 
+  num get long => positioning.last.long; 
+  LatLng get center => new LatLng(lat, long);
   
   void clear (){
     positioning.clear();
